a64f2d7571285a46622e7461e49d0475bacb910f
danielferguson <107422624+danielalexis1860@users.noreply.github.com>
2024-06-05 04:40:35 -0700

    Add files via upload

---
+++ AVOID_INTERSECTING_STATES/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+import Player from './COMPONENTS/Player.jsx';
+import GameBoard from './COMPONENTS/GameBoard.jsx';
+import Log from './COMPONENTS/Log.jsx';
+
+
+function App(){
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare() {
+    //update activePlayer state by calling setActivePlayer   //tenary expression
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns();
+
+  }
+
+    return(
+        <main>
+             <div id = "game-container">
+                <ol id = "players" className="highlight-player"> 
+                  <Player initialName= "Player 1" symbol="X" isActive={activePlayer === 'X'}/> 
+                  <Player initialName= "Player 2" symbol="O" isActive={activePlayer === 'O'}/> 
+                </ol>
+                
+                <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+             </div>
+             Log
+        </main>
+    );
+}
